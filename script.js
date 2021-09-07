bf1db3492483de84c0610e6d784eed0f368de9a6
ElSnako314 <71267560+ElSnako314@users.noreply.github.com>
2021-09-07 14:24:11 -0400

    Buggy

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const $hand = document.getElementById("hand")
 const $firstCard = $hand.children[0]
 const $pile = document.getElementById("pile")
+const $HUD = document.getElementById("HUD")
 
 const colors = ["red", "blue", "green", "yellow"]
 const cards = []
 const players = []
+
 players[0] = {}
 players[0].hand = []
 players[1] = {}
@@ -22,13 +24,53 @@ spawnCards()
 players.forEach(player => dealCards(player))
 placeFirstPileCard()
 
-function takeTurn() {
+function takeTurn(event) {
+    if (turn == 0) {
+        const turnColor = event.target.style.backgroundColor
+        const turnValue = event.target.innerHTML
+        const pileTop = pile[pile.length-1]
+        if (pileTop.value == turnValue || pileTop.color == turnColor) {
+            const turnID = players[0].hand.findIndex(card => card.value == 1 && card.color == "blue")
+            const turnCard = players[0].hand.splice(turnID,1)[0]
+            console.log(turnCard)
+            pile.push(turnCard)
+            turn++
+            if (turn == 4) turn = 0
+            gameHUD()
+        }
+    }
+    else {
+        const thisHand = players[turn].hand
+        for (let i = 0; i < thisHand.length; i++) {
+            const turnID = Math.floor(Math.random()*thisHand.length)
+            const pileTop = pile[pile.length-1]
+            if (thisHand[turnID].value == pileTop.value || thisHand[turnID].color == pileTop.color) {
+                const turnHand = thisHand.splice(turnID,1)[0]
+                pile.push(turnCard)
+                turn++
+                if (turn == 4) turn = 0
+                gameHUD()
+                break;
+            }
+        }
+    }
     showPileTop()
 }
 
 $hand.onclick = takeTurn
+$pile.onclick = takeTurn
 
-function showPileTop(top) {
+function gameHUD() {
+    $HUD.innerHTML = `Who's Turn? ${turn+1}
+    Player 1: ${players[0].hand.length}
+    Player 2: ${players[1].hand.length}
+    Player 3: ${players[2].hand.length}
+    Player 4: ${players[3].hand.length}
+    `
+}
+
+function showPileTop() {
+    const top = pile[pile.length-1]
     const $newCard = document.createElement("div")
     $newCard.classList.add("card")
     $newCard.innerHTML = top.value
